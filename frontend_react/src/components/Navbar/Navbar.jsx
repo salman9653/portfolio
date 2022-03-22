1c90dc581da0aa45a04c30575fdbb97e35f3584f
Salman <salman9915189734@gmail.com>
2022-03-22 14:41:52 +0530

    Navbar compleated

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import './Navbar.scss'
+import React, { useState } from 'react';
+import './Navbar.scss';
+import { images } from '../../constants';
+import { HiMenu, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
     return (
-        <div>
-            Navbar
-        </div>
-    )
+        <nav className='app__navbar'>
+            <div className='app__navbar-logo'>
+                <img src={images.logo} Alt1="logo" />
+            </div>
+            <ul className='app__navbar-links'>
+                {['home', 'about', 'skills', 'work', 'contact'].map((item) => (
+                    <li className='app__flex p-text' key={`link-${item}`}>
+                        <div />
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                ))}
+            </ul>
+            <div className="app__navbar-menu">
+                <HiMenu onClick={() => setToggle(true)} />
+                {toggle && (
+                    <motion.div
+                        whileInView={{ x: [300, 0] }}
+                        transition={{ duration: 0.85, ease: 'easeOut' }}
+                    >
+                        <HiX onClick={() => setToggle(false)} />
+                        <ul>
+                            {['home', 'about', 'skills', 'work', 'contact'].map((item) => (
+                                <li key={item}>
+                                    <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </motion.div>
+                )}
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar
