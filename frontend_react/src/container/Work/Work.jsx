af232f79446048b3081721e3a720563f8c879bb8
Salman <salman9915189734@gmail.com>
2022-03-26 01:49:45 +0530

    styled work section

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -9,18 +9,28 @@ const Work = () => {
     const [activeFilter, setActiveFilter] = useState('All');
     const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
     const [works, setWorks] = useState([]);
-    const [filterWorks, setFilterWorks] = useState([]);
+    const [filterWork, setFilterWork] = useState([]);
 
     useEffect(() => {
         const query = '*[_type == "works"]';
         client.fetch(query)
             .then((data) => {
                 setWorks(data);
-                setFilterWorks(data);
+                setFilterWork(data);
             })
     }, [])
     const handleWorkFilter = (item) => {
+        setActiveFilter(item);
+        setAnimateCard([{ y: 100, opacity: 0 }]);
 
+        setTimeout(() => {
+            setAnimateCard([{ y: 0, opacity: 1 }]);
+            if (item === 'All') {
+                setFilterWork(works);
+            } else {
+                setFilterWork(works.filter((work) => work.tags.includes(item)));
+            }
+        }, 500)
     }
     return (
         <>
@@ -45,21 +55,21 @@ const Work = () => {
                 transition={{ duration: 0.5, delayChildren: 0.5 }}
                 className='app__work-portfolio'
             >
-                {filterWorks.map((work, index) => (
-                    <div className="app__work-item  app__fle" key={index}>
+                {filterWork.map((work, index) => (
+                    <div className="app__work-item  app__flex" key={index}>
                         <div className="app__work-img app__flex">
                             <img src={urlFor(work.imgUrl)} alt={work.name} />
                             <motion.div
                                 whileHover={{ opacity: [0, 1] }}
                                 transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
-                                className="app__work-hover app__fex"
+                                className="app__work-hover app__flex"
                             >
                                 <a href={work.projectLink} target="_blank" rel='noreffer'>
                                     <motion.div
                                         whileInView={{ scale: [0, 1] }}
                                         whileHover={{ scale: [1, 0.9] }}
                                         transition={{ duration: 0.25 }}
-                                        className="app__fex"
+                                        className="app__flex"
                                     >
                                         <AiFillEye />
                                     </motion.div>
@@ -69,7 +79,7 @@ const Work = () => {
                                         whileInView={{ scale: [0, 1] }}
                                         whileHover={{ scale: [1, 0.9] }}
                                         transition={{ duration: 0.25 }}
-                                        className="app__fex"
+                                        className="app__flex"
                                     >
                                         <AiFillGithub />
                                     </motion.div>
